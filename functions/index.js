0e850a575a37244f0eca9bb1fb9f8864ce75b186
ashvinnagarajan <50090261+ashvinnagarajan@users.noreply.github.com>
2020-11-11 22:53:34 -0800

    Created flask webserver

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+        // Grab the current value of what was written to Cloud Firestore.
+        const original = snap.data().original;
+
+        // Access the parameter `{documentId}` with `context.params`
+        functions.logger.log('Uppercasing', context.params.documentId, original);
+
+        const uppercase = original.toUpperCase();
+
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to Cloud Firestore.
+        // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
+        return snap.ref.set({ uppercase }, { merge: true });
+    });
